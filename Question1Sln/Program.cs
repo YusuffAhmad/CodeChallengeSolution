816e71583aa9b211e923f28b850f94a746bca47b
YusuffAhmad <yusuffahmad2005@gmail.com>
2022-08-05 11:53:03 +0100

    Challenge Solution

---
+++ Question1Sln/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+
+namespace QUESTION1SLN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testCase = {2, 2, 4, 7};
+            var answer = CutTheSticks(testCase);
+            foreach (var item in answer)
+            {
+                Console.Write(item);
+            }
+        }
+        static List<int> CutTheSticks(int[] sticks)
+        {
+            int staticIndex = 1;
+            List<int> result = new List<int>();
+            List<int> workingList = new List<int>();
+            result.Add(sticks.Length);
+            Array.Sort(sticks);
+            workingList = sticks.ToList();
+            for(int i = 0; i < workingList.Count; i++)
+            {
+                for(int j = 0; j < workingList.Count-1; j++)
+                {
+                    if (workingList.Count >1)
+                    {
+                        workingList[j+1] -= workingList[staticIndex];
+                    }
+                }
+                workingList.Remove(workingList[staticIndex]);
+                for (int f = 0; f < workingList.Count-1; f++)
+                {
+                    if (workingList[f] == workingList[f+1])
+                    {
+                        workingList.Remove(workingList[f]);
+                    }
+                    break;
+                }
+                result.Add(workingList.Count);
+            }
+            return result;
+        }
+    }
+}
+
---
+++ Question2Sln/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+
+namespace QUESTION2SLN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testCase = {1, 2, 3};
+            Console.WriteLine(WaterContainer(testCase));
+            
+        }
+        static int WaterContainer(int[] arr)
+        {
+            int result = 0;
+            List<int> workingList = new List<int>();
+            Array.Sort(arr);
+            workingList = arr.ToList();
+            result = workingList[^1] * workingList[^1] -1;
+            return result;
+        }
+    }
+}
+
